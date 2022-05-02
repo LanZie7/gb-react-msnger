598ab8cce853e19c3634042ba8f181072a180e91
Бочкова Светлана <bochkova_su@resanta.ru>
2022-05-02 18:52:46 +0300

    redux-thunk connecting

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,18 @@
-import {combineReducers, createStore} from "redux";
+import { applyMiddleware, combineReducers, createStore, compose } from "redux";
 import { profileReducer } from "./profile/reducer";
 import { chatsReducer } from "./chats/reducer";
 import { messagesReducer } from "./messages/reducer";
+import thunk from "redux-thunk";
 
 const rootReducer = combineReducers({
   profile: profileReducer,
   chats: chatsReducer,
   messages: messagesReducer
-})
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
   rootReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  composeEnhancers(applyMiddleware(thunk))
 );
